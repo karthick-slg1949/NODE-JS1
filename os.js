5bb54f841c1fae1639c32092631f7722a27cf5b1
KARTHICK ALAGAR <karthikarthick1949@gmail.com>
2024-08-09 14:19:02 +0530

    tst

---
+++ os.js
@@ -0,0 +1,12 @@
+//  const os = require('os')
+ import os from 'os'
+console.log(os.arch());
+console.log(os.machine());
+console.log(os.platform());
+console.log(os.release());
+console.log(os.freemem());
+console.log(os.totalmem());
+console.log(os.type());
+console.log(os.version());
+console.log(os.hostname());
+console.log("ES-6 Module");
